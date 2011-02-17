58018ae919d6812ea223f8755fc6e2e22fd86999
Matthew Crenshaw <scubasonar@gmail.com>
2011-02-17 10:58:09 -0800

    moved uOLED and Zigbit out to the library

--- PandaInterrogator/PandaInterrogator/Program.cs
+++ PandaInterrogator/PandaInterrogator/Program.cs
@@ -4,49 +4,36 @@ using System.Text;
 using System.IO.Ports;
 using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
-using uOLED;
-
 using GHIElectronics.NETMF.FEZ;
+using GHIElectronics.NETMF.System;
 
+using DSS.Devices;
 namespace PandaInterrogator
 {
     public class Program
     {
         public static void Main()
         {
-            uOLED.uOLED display;
-            SerialPort disp = new SerialPort("COM2", 9600);
-            disp.Open();
-            SerialPort radio = new SerialPort("COM1", 38400, Parity.None, 8);
-            
-            Random r = new Random();
             bool ledState = false;
-
             OutputPort led = new OutputPort((Cpu.Pin)FEZ_Pin.Digital.LED, ledState);
-
+            uOLED display = new uOLED(new SerialPort("COM2"));
+            ZigBit radio = new ZigBit("COM1");
+            Random r = new Random();
             
-            disp.ReadTimeout = 100;
-            radio.Open();
+
+            //SerialPort radio = new SerialPort("COM1", 38400, Parity.None, 8);
+            
+            //radio.Open();
 
             // display init
             Thread.Sleep(50);
-            display = new uOLED.uOLED(disp);
+            
             display.Cls();
             Thread.Sleep(50);
             bool value = display.DrawPolygon(3, new byte[] { 32, 0, 0, 32, 50, 32 }, new byte[] { 0xFF, 0xFF });
             //
 
             //radio init
-            radio.Write(Encoding.UTF8.GetBytes("ATX\n"), 0, 4);
-            Thread.Sleep(5);
-            radio.Write(Encoding.UTF8.GetBytes("AT+GSN=1\n"), 0, 9);
-            Thread.Sleep(5);
-            radio.Write(Encoding.UTF8.GetBytes("AT+WPANID=1620\n"), 0, 15);
-            Thread.Sleep(5);
-            radio.Write(Encoding.UTF8.GetBytes("AT+WROLE=0 +WSRC=0\n"), 0, 19);
-            Thread.Sleep(5);
-            radio.Write(Encoding.UTF8.GetBytes("AT+WJOIN\n"), 0, 9);
-            Thread.Sleep(5);
             while (true)
             {
                 bool x;
@@ -56,6 +43,12 @@ namespace PandaInterrogator
                 Thread.Sleep(300);
                 x = display.DrawButtonTXT(0x00, 0, 0, new byte[] { 0x0F, 0xBF }, 0x00, new byte[] { 0xCF, 0xDF }, 1, 1, "hello");
                 Thread.Sleep(300);
+            }
+
+            while (true)
+            {
+                int i = 0;
+                Thread.Sleep(100);
             }
         }
 
